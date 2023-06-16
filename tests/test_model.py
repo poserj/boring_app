26851340881d5742a3f80c3fa8d6a7e593cd83b2
PSN <ya.ry>
2023-06-16 15:45:50 +0300

    fixed tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,23 +1,23 @@
 import unittest
 
-from main import get_similar
+from helpers import get_similar
 
 
 class TestSimilarity(unittest.TestCase):
     def test_equal(self):
         a = [1, "test", 23, 0.9]
         b = [1, "test", 23, 0.9]
-        self.assertEqual(get_similar([a, b, b]), [1.0, b])
+        self.assertEqual(get_similar([a, b, b]), {'matcher': 1.0,  'activity': b})
 
     def test_not_equal(self):
         a = [7, "relax", '23', 0]
         b = [1, "test", 23, 0.9]
-        self.assertEqual(get_similar([a, b, b]), [0.0, b])
+        self.assertEqual(get_similar([a, b, b]), {'matcher': 0.0,  'activity': b})
 
     def test_partial_equal(self):
         a = [7, "relax", 23, 0]
         b = [7, "test", 23, 0.9]
-        self.assertEqual(get_similar([a, b, b]), [0.5, b])
+        self.assertEqual(get_similar([a, b, b]), {'matcher': 0.5,  'activity': b})
 
 
 if __name__ == '__main__':
